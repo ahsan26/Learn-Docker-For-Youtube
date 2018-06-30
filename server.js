5c3ccde68de9ab9c6430ffd9aeb2df324c52ad54
ahsan26 <ahsanusmanabduljabbar@gmail.com>
2018-06-30 23:33:24 +0500

    added the docker file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(19000, function(){
+app.listen(8000, function(){
     console.log("Server is Started!");
 });
